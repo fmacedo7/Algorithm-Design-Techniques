2455da66cb7fd4501b7800be922dd3d99c3661a2
Felizberto Macedo <felizberto11fred@gmail.com>
2023-01-12 11:56:23 -0300

    atualizei o algoritmo de geração de senhas, ela retorna uma senha de tamanho 'x' predefinido, no caso, oito.

--- Backtracking/geracao_senha.py
+++ Backtracking/geracao_senha.py
@@ -1,16 +1,11 @@
 import random
 
 def gera_senha(tamanho, senha_atual, caracteres_permitidos):
-    # Se a senha atual tem o tamanho desejado, retorna a senha
     if len(senha_atual) == tamanho:
         return senha_atual
-    # embaralha a lista de caracteres permitidos
     random.shuffle(caracteres_permitidos)
-    # para cada caractere permitido
     for caractere in caracteres_permitidos:
-        # chama recursivamente a função passando a senha atual mais o caractere 
         nova_senha = gera_senha(tamanho, senha_atual + caractere, caracteres_permitidos)
-        # se a nova senha tiver o tamanho desejado, retorna a senha
         if len(nova_senha) == tamanho:
             return nova_senha
     return senha_atual
