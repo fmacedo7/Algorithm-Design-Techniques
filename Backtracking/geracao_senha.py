dcfc5e7c4631fd2ac6e8d2c67e979c90c3e08d89
Felizberto Macedo <felizberto11fred@gmail.com>
2023-01-13 00:27:43 -0300

    pequenas modificações

--- Backtracking/geracao_senha.py
+++ Backtracking/geracao_senha.py
@@ -1,16 +1,12 @@
 import random
 
-def gera_senha(tamanho, senha_atual, caracteres_permitidos):
-    if len(senha_atual) == tamanho:
-        return senha_atual
-    random.shuffle(caracteres_permitidos)
-    for caractere in caracteres_permitidos:
-        nova_senha = gera_senha(tamanho, senha_atual + caractere, caracteres_permitidos)
-        if len(nova_senha) == tamanho:
-            return nova_senha
-    return senha_atual
+def gera_senhas(senha, caracteres, tamanho):
+    if len(senha) == tamanho:
+        print(senha)
+    else:
+        for caractere in caracteres:
+            gera_senhas(senha + caractere, caracteres, tamanho)
 
-caracteres = list("abcdefghijklmnopqrstuvwxyz0123456789")
-tamanho = 8
-senha = gera_senha(tamanho, "", caracteres)
-print(senha)
+caracteres = "abcdefghijklmnopqrstuvwxyz0123456789"
+tamanho = 4
+gera_senhas("", caracteres, tamanho)
