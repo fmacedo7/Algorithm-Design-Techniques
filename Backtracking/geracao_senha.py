fa806e9f95ba3ed7b404c20bfa25b04cf3556fd2
Felizberto Macedo <felizberto11fred@gmail.com>
2023-01-19 23:09:47 -0300

    backtracking atualizado

--- Backtracking/geracao_senha.py
+++ Backtracking/geracao_senha.py
@@ -1,12 +1,11 @@
-import random
 
 def gera_senhas(senha, caracteres, tamanho):
     if len(senha) == tamanho:
         print(senha)
     else:
         for caractere in caracteres:
-            gera_senhas(senha + caractere, caracteres, tamanho)
+            gera_senhas(senha + caractere, caracteres, tamanho) #chamada recursiva
 
-caracteres = "abcdefghijklmnopqrstuvwxyz0123456789"
-tamanho = 4
+caracteres = "abcdefghijklmnopqrstuvwxyz0123456789" #caracteres disponiveis para gerar senhas
+tamanho = 2 #tamanho das senhas
 gera_senhas("", caracteres, tamanho)
